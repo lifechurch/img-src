3a207e8deed923fd5e2311baf0e0e8378e95660c
Sam Carlton <sam@sam.lc>
2018-05-05 11:31:45 -0500

    Add name and defaultValue props to TextInput

--- src/components/text-input/index.js
+++ src/components/text-input/index.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types'
 
 function TextInput(props) {
 	return (
-		<input type='text' {...props} className={`input-reset br3 bg-white pa3 outline-0 ${props.className}`} />
+		<input
+			type='text'
+			{...props}
+			name={props.name}
+			defaultValue={props.defaultValue}
+			className={`input-reset br3 bg-white pa3 outline-0 ${props.className}`}
+		/>
 	)
 }
 
