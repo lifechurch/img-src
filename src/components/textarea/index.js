89705be80fef1e928567437e837cf8e4b72b5f8b
alex-gale <alexgaleiii@gmail.com>
2018-05-07 10:28:22 +0100

    Added styling to match images - Added a style const for the resize property - Added a stylesheet for scrollbar

--- src/components/textarea/index.js
+++ src/components/textarea/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import './index.css'
+
+const styles = {
+	resize: 'none'
+}
 
 function TextArea(props) {
 	return (
-		<textarea {...props} className="br2 pa2 w-100 h3 input-reset b-moon-gray outline-0" />
+		<textarea {...props} style={styles} className="bg-animate br2 pa2 w-100 h3 input-reset b--moon-gray outline-0" />
 	)
 }
 
