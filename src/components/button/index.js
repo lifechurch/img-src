04c9a543ce2afe2c7e538ac93cc3cffc53d16dab
alex-gale <alexgaleiii@gmail.com>
2018-05-07 18:11:00 +0100

    Added styling based on props * Button will render differently depending on its 'render' prop * Button will not be usable if the 'disabled' prop is 'true' * Added a test

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,13 +2,40 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-function Button(props) {
-	const classes = 'pointer h2 br-pill bg-mid-gray white ph3 pv1 no-underline'
+function Button({ to, onClick, ...props }) {
+	const classes = ['pointer', 'link', 'h2', 'br-pill', 'ph3', 'pv1']
 
-	return props.to ? (
-		<Link {...props} className={classes}>{props.children}</Link>
+	classes.push(
+		props.render === 'solid' ? 'bg-mid-gray white' : 'ba mid-gray',
+		props.disabled ? 'o-50' : 'dim'
+	)
+
+	if (props.disabled) {
+		return (
+			<a {...props} className={classes.join(' ')}>{props.children}</a>
+		)
+	}
+
+	return to ? (
+		<Link
+			{...props}
+			to={to}
+			onClick={onClick}
+			className={classes.join(' ')}
+		>
+			{props.children}
+		</Link>
 	) : (
-		<a {...props} className={classes}>{props.children}</a>
+		<a
+			{...props}
+			onClick={onClick}
+			role='button'
+			tabIndex={0}
+			onKeyDown={onClick}
+			className={classes.join(' ')}
+		>
+			{props.children}
+		</a>
 	)
 }
 
@@ -16,14 +43,16 @@ Button.propTypes = {
 	children: PropTypes.node,
 	to: PropTypes.node,
 	disabled: PropTypes.bool,
-	render: PropTypes.oneOf(['solid', 'outline-only'])
+	render: PropTypes.oneOf(['solid', 'outline-only']),
+	onClick: PropTypes.func
 }
 
 Button.defaultProps = {
 	children: null,
 	to: null,
 	disabled: false,
-	render: 'solid'
+	render: 'solid',
+	onClick: null
 }
 
 export default Button
--- src/containers/user-registration/index.js
+++ src/containers/user-registration/index.js
@@ -15,7 +15,7 @@ function UserRegistration() {
 				<input type="text" placeholder="LAST NAME" /><br />
 				Why do you want to be on the volunteering team for YouVersion?<br />
 				<TextArea />
-				<Button to='/' onClick={() => alert('hey')}>Submit</Button>
+				<Button to='/' render='solid' onClick={() => alert('hey')}>Sign In</Button>
 			</Card>
 			<Card>
 				Another card
