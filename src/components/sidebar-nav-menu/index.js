7573850ea79e416497eb9c30f7749aa1069183d4
Alex Gale <alexgaleiii@gmail.com>
2018-06-07 12:43:18 +0100

    Created basic sidebar layout
    * Imported icon assets
    * Modified the existing component to match designs

--- src/components/sidebar-nav-menu/index.js
+++ src/components/sidebar-nav-menu/index.js
@@ -1,11 +1,64 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+import { FormattedMessage, injectIntl } from 'react-intl'
+import YVLogo from '../../assets/LC_YouVersion_Logo_with_icon_Light.png'
+import AdminIcon from '../../assets/admin.svg'
+import ToDoIcon from '../../assets/todo.svg'
+import MyImagesIcon from '../../assets/me.svg'
 
-function SidebarNavMenu() {
+function SidebarNavMenu(props) {
 	return (
-		<div className="pa4">
-			<h3 className="ma0 pa0">Sidebar Nav Menu</h3>
+		<div className="pa3 f3">
+			<img src={YVLogo} alt="YouVersion" width={200} className="mb4" />
+
+			{props.isAdmin &&
+				<div className="mb3">
+					<div className="pl3 mb3">
+						<NavLink className="no-underline light-silver link dim flex align-center" activeClassName="mid-gray" to="/admin">
+							<img src={AdminIcon} alt="" className="mr3" />
+							<FormattedMessage id="admin" />
+						</NavLink>
+					</div>
+
+					<div className="pl3 ml5 mb3">
+						<NavLink className="no-underline light-silver link dim flex align-center" activeClassName="mid-gray" to="/admin/pending">
+							<FormattedMessage id="pending" />
+						</NavLink>
+					</div>
+					<div className="pl3 ml5 mb3">
+						<NavLink className="no-underline light-silver link dim flex align-center" activeClassName="mid-gray" to="/admin/images">
+							<FormattedMessage id="images" />
+						</NavLink>
+					</div>
+
+					<div className="w-100 bg-mid-gray" style={{ height: 1 }} />
+				</div>
+			}
+
+			<div className="pl3 mb3">
+				<NavLink className="no-underline light-silver link dim flex align-center" activeClassName="mid-gray" to="/todo">
+					<img src={ToDoIcon} alt="" className="mr3" />
+					<FormattedMessage id="todo" />
+				</NavLink>
+			</div>
+
+			<div className="pl3 mb3">
+				<NavLink className="no-underline light-silver link dim flex align-center" activeClassName="mid-gray" to="/myimages">
+					<img src={MyImagesIcon} alt="" className="mr3" />
+					<FormattedMessage id="myImages" />
+				</NavLink>
+			</div>
 		</div>
 	)
 }
 
-export default SidebarNavMenu
+SidebarNavMenu.propTypes = {
+	isAdmin: PropTypes.bool
+}
+
+SidebarNavMenu.defaultProps = {
+	isAdmin: true
+}
+
+export default injectIntl(SidebarNavMenu)
--- src/layouts/sidebar-nav/index.js
+++ src/layouts/sidebar-nav/index.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 
 function SidebarNav({ menu, children }) {
 	return children ? (
-		<div className="pa4 h-100">
+		<div className="h-100">
 			<div className="cf h-100">
-				<div className="fl w-20 bg-moon-gray h-100">
+				<div className="fl w7 br b--moon-gray h-100">
 					{menu}
 				</div>
-				<div className="fl w-80">
+				<div className="pa4 fl w-80">
 					{children}
 				</div>
 			</div>
