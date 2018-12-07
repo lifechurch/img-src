8f931d187db3fd911866a7ab4e590d408e985c72
Alex Gale <alexgaleiii@gmail.com>
2018-12-07 17:42:44 +0000

    Started integrating API
    * Moved page into component (as opposed to function)
    * Started integration of API into page

--- src/containers/user-profile/index.js
+++ src/containers/user-profile/index.js
@@ -2,68 +2,80 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 
+import Partner from '../../tupos/models/partner'
 import ToggleBar from '../../components/toggle-bar'
 import SecondaryHeading from './../../components/typography/secondary-heading'
 import BodyText from './../../components/typography/body-text'
 
-function UserProfile(props) {
-	const {
-		match: {
-			params: {
-				userId
-			},
-		}
-	} = props
+class UserProfile extends React.Component {
+  componentDidMount() {
+    this.loadData()
+  }
 
-	return (
-		<div className="flex flex-column w-100 min-h-100">
-			<div className="flex justify-center pt4 ph4">
-				<div className="flex flex-column items-center mb4">
-					<img className="br-100 w4 h4 mb3" alt="" />
-					<SecondaryHeading className="ma0 pa0">
-						name
-					</SecondaryHeading>
-					<div className="mt2 tc">
-						<BodyText>
-							<FormattedMessage id="youversionVolunteer" />
-						</BodyText>
-						<BodyText>
-							<FormattedMessage id="designerSince" />
-						</BodyText>
-					</div>
-				</div>
-			</div>
+  async loadData() {
+    let partners = await Partner.getOne(1)
+    console.log(partners)
+  }
 
-			<div className="flex-auto pa3 bg-light-gray">
-				<div className="w-100 flex flex-column items-center ma3">
-					<ToggleBar
-						links={[
-							{
-								text: 'Submissions ()',
-								address: `/user-profile/${userId}/submissions`
-							},
-							{
-								text: 'Approved ()',
-								address: `/user-profile/${userId}/approved`
-							},
-							{
-								text: 'Declined ()',
-								address: `/user-profile/${userId}/declined`
-							},
-							{
-								text: 'Pending ()',
-								address: `/user-profile/${userId}/pending`
-							}
-						]}
-					/>
+  render() {
+    const {
+  		match: {
+  			params: {
+  				userId
+  			},
+  		}
+  	} = this.props
 
-					<div className="w-100 mt4">
-						{}
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+    return (
+  		<div className="flex flex-column w-100 min-h-100">
+  			<div className="flex justify-center pt4 ph4">
+  				<div className="flex flex-column items-center mb4">
+  					<img className="br-100 w4 h4 mb3" alt="" />
+  					<SecondaryHeading className="ma0 pa0">
+  						name
+  					</SecondaryHeading>
+  					<div className="mt2 tc">
+  						<BodyText>
+  							<FormattedMessage id="youversionVolunteer" />
+  						</BodyText>
+  						<BodyText>
+  							<FormattedMessage id="designerSince" />
+  						</BodyText>
+  					</div>
+  				</div>
+  			</div>
+
+  			<div className="flex-auto pa3 bg-light-gray">
+  				<div className="w-100 flex flex-column items-center ma3">
+  					<ToggleBar
+  						links={[
+  							{
+  								text: 'Submissions ()',
+  								address: `/user-profile/${userId}/submissions`
+  							},
+  							{
+  								text: 'Approved ()',
+  								address: `/user-profile/${userId}/approved`
+  							},
+  							{
+  								text: 'Declined ()',
+  								address: `/user-profile/${userId}/declined`
+  							},
+  							{
+  								text: 'Pending ()',
+  								address: `/user-profile/${userId}/pending`
+  							}
+  						]}
+  					/>
+
+  					<div className="w-100 mt4">
+  						{}
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  	)
+  }
 }
 
 UserProfile.propTypes = {
