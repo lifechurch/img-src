7c3ddcc2d2cd6dcd483c3dae14d733c621c2d620
Ezrah Nyanat <ezrah.nyanat@life.church>
2019-01-07 12:51:24 -0600

    RE-tupos for Image Status Counts model

--- src/tupos/models/image/index.js
+++ src/tupos/models/image/index.js
@@ -1,6 +1,4 @@
 import { TuposModel } from '@youversion/tupos-base'
-import setDate from '@youversion/tupos-base/dist/setters/date'
-import setNumber from '@youversion/tupos-base/dist/setters/number'
 import setString from '@youversion/tupos-base/dist/setters/string'
 import api4 from '@youversion/tupos-base/dist/fetchers/api4'
 
@@ -13,31 +11,13 @@ class Image extends TuposModel {
 	constructor(json) {
 		super(json)
 		if (!json || typeof json !== 'object') return
-		this.versionId = json.version_id
-		this.usfm = json.usfm
-		this.createdDt = json.created_dt
-		this.url = json.url
-		this.userId = json.user_id
-		this.status = json.status
-		this.languageTag = json.language_tag
-		this.weight = json.weight
-		this.id = json.id
-		this.category = json.category
+		this.errors = json.errors
 	}
 
 	/** Convert Image to simple object */
 	toObject() {
 		return {
-			version_id: this.versionId,
-			usfm: this.usfm,
-			created_dt: this.createdDt,
-			url: this.url,
-			user_id: this.userId,
-			status: this.status,
-			language_tag: this.languageTag,
-			weight: this.weight,
-			id: this.id,
-			category: this.category
+			errors: this.errors
 		}
 	}
 
@@ -101,94 +81,13 @@ class Image extends TuposModel {
 	}
 
 
-	/** @type {number} */
-	get versionId() {
-		return this._versionId
-	}
-
-	set versionId(versionId) {
-		this._versionId = setNumber(versionId, 'versionId')
-	}
-
 	/** @type {string} */
-	get usfm() {
-		return this._usfm
+	get errors() {
+		return this._errors
 	}
 
-	set usfm(usfm) {
-		this._usfm = setString(usfm, 'usfm')
-	}
-
-	/** @type {Date} */
-	get createdDt() {
-		return this._createdDt
-	}
-
-	set createdDt(createdDt) {
-		this._createdDt = setDate(createdDt, 'createdDt')
-	}
-
-	/** @type {string} */
-	get url() {
-		return this._url
-	}
-
-	set url(url) {
-		this._url = setString(url, 'url')
-	}
-
-	/** @type {number} */
-	get userId() {
-		return this._userId
-	}
-
-	set userId(userId) {
-		this._userId = setNumber(userId, 'userId')
-	}
-
-	/** @type {string} */
-	get status() {
-		return this._status
-	}
-
-	set status(status) {
-		this._status = setString(status, 'status')
-	}
-
-	/** @type {string} */
-	get languageTag() {
-		return this._languageTag
-	}
-
-	set languageTag(languageTag) {
-		this._languageTag = setString(languageTag, 'languageTag')
-	}
-
-	/** @type {string} */
-	get weight() {
-		return this._weight
-	}
-
-	set weight(weight) {
-		this._weight = setString(weight, 'weight')
-	}
-
-	/** @type {number} */
-	get id() {
-		return this._id
-	}
-
-	set id(id) {
-		this._id = setNumber(id, 'id')
-	}
-
-	/** @type {string} */
-	get category() {
-		return this._category
-	}
-
-	set category(category) {
-		this._category = setString(category, 'category')
+	set errors(errors) {
+		this._errors = setString(errors, 'errors')
 	}
 
 }
